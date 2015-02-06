fd8cda10827375b832f3b56a2841bc33e634aed6
Viliam Rockai <vrockai@redhat.com>
2015-02-06 17:43:05 +0100

    HAWKULAR-13: resource layer for hawkular-metrics rest

---
+++ hawkular-rest-service/dist/hawkular-rest-service.js
@@ -0,0 +1,77 @@
+/// <reference path="../bower_components/dt-angular/angular.d.ts" />
+/**
+ * @ngdoc provider
+ * @name hawkular.rest.HawkularRest
+ * @description
+ * # HawkularRest
+ * Provider in the hawkular.rest.
+ */
+var hawkularRest;
+(function (hawkularRest) {
+    hawkularRest.hawkMetric = angular.module('hawkular.rest', ['ngResource']);
+    hawkularRest.hawkMetric.provider('HawkularRest', function () {
+        // time (in ms) the notifications are shown
+        this.host = 'localhost';
+        this.port = 8080;
+        this.setHost = function (host) {
+            this.host = host;
+            return this;
+        };
+        this.setPort = function (port) {
+            this.port = port;
+            return this;
+        };
+        this.$get = ['$resource', function ($resource) {
+            var prefix = 'http://' + this.host + ':' + this.port;
+            var factory = {};
+            factory['Tenant'] = $resource(prefix + '/rhq-metrics/tenants', {});
+            factory['Metric'] = $resource(prefix + '/rhq-metrics/:tenantId/metrics', {
+                tenantId: '@tenantId'
+            }, {
+                queryNum: {
+                    method: 'GET',
+                    isArray: true,
+                    params: { type: 'num' }
+                },
+                queryAvail: {
+                    method: 'GET',
+                    isArray: true,
+                    params: { type: 'avail' }
+                },
+                queryLog: {
+                    method: 'GET',
+                    isArray: true,
+                    params: { type: 'log' }
+                }
+            });
+            factory['NumericMetric'] = $resource(prefix + '/rhq-metrics/:tenantId/metrics/numeric', {
+                tenantId: '@tenantId'
+            });
+            factory['NumericMetricData'] = $resource(prefix + '/rhq-metrics/:tenantId/metrics/numeric/:numericId/data', {
+                tenantId: '@tenantId',
+                numericId: '@numericId'
+            });
+            factory['NumericMetricMeta'] = $resource(prefix + '/rhq-metrics/:tenantId/metrics/numeric/:numericId/meta', {
+                tenantId: '@tenantId',
+                numericId: '@numericId'
+            }, {
+                update: 'PUT'
+            });
+            factory['NumericMetricMultiple'] = $resource(prefix + '/rhq-metrics/:tenantId/metrics/numeric/data', {
+                tenantId: '@tenantId',
+                numericId: '@numericId'
+            });
+            factory['AvailabilityMetric'] = $resource(prefix + '/rhq-metrics/:tenantId/metrics/availability', {
+                tenantId: '@tenantId'
+            });
+            factory['AvailabilityMetricData'] = $resource(prefix + '/rhq-metrics/:tenantId/metrics/availability/:availabilityId/data', {
+                tenantId: '@tenantId',
+                availabilityId: '@availabilityId'
+            });
+            factory['AvailabilityMetricMultiple'] = $resource(prefix + '/rhq-metrics/:tenantId/metrics/availability/data', {
+                tenantId: '@tenantId'
+            });
+            return factory;
+        }];
+    });
+})(hawkularRest || (hawkularRest = {}));
