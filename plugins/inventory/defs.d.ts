a45a87549725adc73e3369793dc455b8e7d3565a
Viliam Rockai <vrockai@redhat.com>
2015-02-23 14:43:10 +0100

    HAWKULAR-37: Removed inventory scripts

--- plugins/inventory/defs.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="d.ts/includes.d.ts"/>
-/// <reference path="d.ts/inventory/ts/inventoryGlobals.d.ts"/>
-/// <reference path="d.ts/inventory/ts/inventoryPlugin.d.ts"/>
-/// <reference path="d.ts/inventory/ts/inventory.d.ts"/>
